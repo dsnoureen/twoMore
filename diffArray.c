64051bf51fb2267ea3466ab398aa726c0254960e
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-08-28 20:46:03 +0600

    diffArray

--- diffArray.c
+++ diffArray.c
@@ -1,23 +1,56 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 int main()
 {
     int testcase;
     scanf("%d", &testcase);
-    for (int i = 0; i < testcase; i++)
+    int i;
+    for (i = 0; i < testcase; i++)
     {
         int n;
         scanf("%d", &n);
         int a[n];
-        for (int i = 0; i < n; i++)
+        int b[n], c[n], d[n];
+        for (i = 0; i < n; i++)
         {
             scanf("%d", &a[i]);
         }
-        int b[n], c[n];
-        for (int i = 0; i < n; i++)
+
+        // COPYING COPYING OF a array
+        for (i = 0; i < n; i++)
         {
-            printf("%d ", a[i]);
+            b[i] = a[i];
         }
-        printf("\n");
+
+        // Ascending the elements in b array
+        i=0;
+        int j=n-1;
+        while(i<j)
+        {
+            if (b[i] > b[j])
+            {
+                int temp = b[i];
+                b[i] = b[j];
+                b[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+for (i = 0; i < n; i++)
+        {
+            printf("%d ", b[i]);
+        }
+
+
+        // SUBTRACTION in c:
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         printf("%d ", c[i] = abs(a[i] - b[i]));
+    //     }
+    //     printf("\n");
+    // }
     }
     return 0;
 }
