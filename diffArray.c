1500db35db457d616ebe9be52de40bd0f860ef5b
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-08-28 21:18:00 +0600

    kaj hoyna

--- diffArray.c
+++ diffArray.c
@@ -5,52 +5,48 @@ int main()
 {
     int testcase;
     scanf("%d", &testcase);
-    int i;
+    int i, j;
     for (i = 0; i < testcase; i++)
     {
         int n;
         scanf("%d", &n);
-        int a[n];
-        int b[n], c[n], d[n];
+        int a[n], b[n], c[n];
         for (i = 0; i < n; i++)
         {
             scanf("%d", &a[i]);
         }
 
-        // COPYING COPYING OF a array
+        // COPYING OF a array
         for (i = 0; i < n; i++)
         {
             b[i] = a[i];
         }
 
         // Ascending the elements in b array
-        i = 0;
-        int j = n - 1;
-        while (i < j)
+        for (i = 0; i < n - 1; i++)
         {
-            if (b[i] > b[j])
+            for (j = n - 1; j > 0; j--)
             {
-                int temp = b[i];
-                b[i] = b[j];
-                b[j] = temp;
-                i++;
-                j--;
+                if (b[i] > b[j])
+                {
+                    int temp = b[i];
+                    b[i] = b[j];
+                    b[j] = temp;
+                }
             }
         }
 
-        
         for (i = 0; i < n; i++)
         {
             printf("%d ", b[i]);
         }
-
+        printf("\n");
         // SUBTRACTION in c:
-        //     for (int i = 0; i < n; i++)
-        //     {
-        //         printf("%d ", c[i] = abs(a[i] - b[i]));
-        //     }
-        //     printf("\n");
-        // }
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", c[i] = abs(a[i] - b[i]));
+        }
+        printf("\n");
     }
     return 0;
 }
